d8b53bfa874881958b2c393f696256531afd0fe4
Eldad Zipori <eldadzz@student.ubc.ca>
2024-10-15 18:42:55 -0700

    not much

--- kern/file/abstractfile.c
+++ kern/file/abstractfile.c
@@ -1,11 +1,21 @@
 #include <types.h>
+#include <current.h>
+#include <proc.h>
+#include <spinlock.h>
 #include <abstractfile.h>
+#include <vfs.h>
+#include <vnode.h>
 
-int open(const char *filename, int flags)
-{
-
-}
 int open(const char *filename, int flags, mode_t mode)
 {
+    // This operation must be atomic 
+    spinlock_acquire(&curproc->p_lock);
 
+    /*
+        Check if the an identical vnode already exists
+        if not call open from vfs
+        
+    */
+
+    spinlock_release(&curproc->p_lock);
 }
