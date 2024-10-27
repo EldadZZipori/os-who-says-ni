5b327403d0ddc7ac7d9e49dfddcf7f67752715b0
Eldad Zipori <eldadzz@student.ubc.ca>
2024-10-26 17:05:44 -0700

    remove comments

--- kern/syscall/fork.c
+++ kern/syscall/fork.c
@@ -45,15 +45,6 @@ sys_fork(struct trapframe tf, int *retval)
         return ENPROC;
     }
 
-    // TODO Assignment 5: do we need to copyin a trapframe? 
-    // copy user-level trapframe to kernel stack var 
-    // err = copyin((const_userptr_t)tf, &parent_tf, sizeof(struct trapframe));
-    // if (err) 
-    // {
-    //     lock_release(kproc_table->pid_lk);
-    //     return err;
-    // }
-
     parent_tf = tf;
 
     // check user doesn't already have too many processes
@@ -85,22 +76,6 @@ sys_fork(struct trapframe tf, int *retval)
         proc_destroy(new_proc);
         return ENOMEM;
     }
-
-    // 3. copy architectural state - in tf
-    // struct trapframe *child_tf = kmalloc(sizeof(struct trapframe));
-    // if (child_tf == NULL) {
-    //     lock_release(kproc_table->pid_lk);
-    //     proc_destroy(new_proc);
-    //     return ENOMEM;
-    // }
-
-    // err = copy_trapframe(&parent_tf, child_tf);
-    // if (err) {
-    //     lock_release(kproc_table->pid_lk);
-    //     proc_destroy(new_proc);
-    //     kfree(child_tf);
-    //     return err;
-    // }
 
     // add to proctable
     err = pt_add_proc(new_proc, pid);
@@ -148,55 +123,3 @@ child_return(void* data1, unsigned long data2)
 
     enter_forked_process(tf);
 }
-// /**
-//  * @brief private function for copying trapframe 
-//  * 
-// */
-// static int 
-// copy_trapframe(struct trapframe* old_tf, struct trapframe* new_tf)
-// {
-//     if (old_tf == NULL || new_tf == NULL)
-//     {
-//         return EINVAL;
-//     }
-
-//     new_tf->tf_vaddr = old_tf->tf_vaddr;
-//     new_tf->tf_status = old_tf->tf_status;
-//     new_tf->tf_cause = old_tf->tf_cause;
-//     new_tf->tf_lo = old_tf->tf_lo;
-//     new_tf->tf_hi = old_tf->tf_hi;
-//     new_tf->tf_ra = old_tf->tf_ra;
-//     new_tf->tf_at = old_tf->tf_at;
-//     new_tf->tf_v0 = old_tf->tf_v0;
-//     new_tf->tf_v1 = old_tf->tf_v1;
-//     new_tf->tf_a0 = old_tf->tf_a0;
-//     new_tf->tf_a1 = old_tf->tf_a1;
-//     new_tf->tf_a2 = old_tf->tf_a2;
-//     new_tf->tf_a3 = old_tf->tf_a3;
-//     new_tf->tf_t0 = old_tf->tf_t0;
-//     new_tf->tf_t1 = old_tf->tf_t1;
-//     new_tf->tf_t2 = old_tf->tf_t2;
-//     new_tf->tf_t3 = old_tf->tf_t3;
-//     new_tf->tf_t4 = old_tf->tf_t4;
-//     new_tf->tf_t5 = old_tf->tf_t5;
-//     new_tf->tf_t6 = old_tf->tf_t6;
-//     new_tf->tf_t7 = old_tf->tf_t7;
-//     new_tf->tf_s0 = old_tf->tf_s0;
-//     new_tf->tf_s1 = old_tf->tf_s1;
-//     new_tf->tf_s2 = old_tf->tf_s2;
-//     new_tf->tf_s3 = old_tf->tf_s3;
-//     new_tf->tf_s4 = old_tf->tf_s4;
-//     new_tf->tf_s5 = old_tf->tf_s5;
-//     new_tf->tf_s6 = old_tf->tf_s6;
-//     new_tf->tf_s7 = old_tf->tf_s7;
-//     new_tf->tf_t8 = old_tf->tf_t8;
-//     new_tf->tf_t9 = old_tf->tf_t9;
-//     new_tf->tf_k0 = old_tf->tf_k0;
-//     new_tf->tf_k1 = old_tf->tf_k1;
-//     new_tf->tf_gp = old_tf->tf_gp;
-//     new_tf->tf_sp = old_tf->tf_sp;
-//     new_tf->tf_s8 = old_tf->tf_s8;
-//     new_tf->tf_epc = old_tf->tf_epc;
-
-//     return 0;
-// }
