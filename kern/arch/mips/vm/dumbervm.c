3b2ded37e8f7e4b7430d70fb4d493cb10ccb47b5
Eldad Zipori <eldadzz@student.ubc.ca>
2024-12-04 18:25:30 -0800

    small cahnge

--- kern/arch/mips/vm/dumbervm.c
+++ kern/arch/mips/vm/dumbervm.c
@@ -207,7 +207,7 @@ write_stolen_page_to_swap(struct addrspace* as, off_t swap_location, paddr_t sto
 	uio.uio_space = as;
 
 	int result = VOP_WRITE(dumbervm.swap_space, &uio);
-	return result
+	return result;
 
 }
 
@@ -247,26 +247,14 @@ alloc_upages(struct addrspace* as, vaddr_t* va, unsigned npages, int readable, i
 		vaddr_t kseg0_va;
 		kseg0_va = alloc_kpages(1);
 
-		if (kseg0_va == 0) // in this case we should allocate memory from the swap space
-		{
-			off_t swap_location = alloc_swap_page(); // find free area in swap space
-			vaddr_t swapable_page = find_swapable_page(as); // find a page that belongs to the user so we can steal it
-			int s_vpn1 = VADDR_GET_VPN1(swapable_page);
-    		int s_vpn2 = VADDR_GET_VPN2(swapable_page);
-			vaddr_t* s_llpt= as->ptbase[s_vpn1]; // get the original llpte
-			paddr_t stolen_page = s_llpt[s_vpn2]; 
-			paddr_t stolen_ppn = LLPTE_MASK_PPN(stolen_page);
-
-			s_llpt[s_vpn2] = LLPTE_SET_SWAP_BIT((swap_location << 12) | (stolen_page & 0xfff)); // mark the stolen page as swap space and space its location in the swap
-
-			
-
-
-		}
-
 		// set the 'otherpages' field in the memlist node of the first page in the block
 
 		paddr_t pa = KSEG0_VADDR_TO_PADDR(kseg0_va);          // Physical address of the new block we created.
+		if (kseg0_va == 0) // in this case we should allocate memory from the swap space
+		{
+			off_t swap_location = alloc_swap_page(); // find free area in swap space
+			pa = swap_location;
+		}
 
 		// write valid bit
 		
